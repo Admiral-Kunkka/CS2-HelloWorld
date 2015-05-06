6efc3b7e0bb095eeec85c645eeedf8b8a1f6cda2
Admiral-Kunkka <jacob.contreras96@gmail.com>
2015-05-06 08:55:48 -0700

    Added Scanner

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,13 @@
-public class HelloWorld{
+import java.util.*;
+
+public class HelloWorld {
+
+  private static Scanner in = new Scanner(System.in);
+
   public static void main(String[] args){
     System.out.println("Hello World!");
+    System.out.println("Whats your name? >");
+    String name = in.nextLine();
+    System.out.println("name:" + name );
   }
-
 }
